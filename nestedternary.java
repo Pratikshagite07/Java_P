cdd6e2308288b10c5c9ef5e7317433f82e7ba145
Pratiksha gite <118196235+Pratikshagite07@users.noreply.github.com>
2023-04-10 18:09:42 +0530

    Add files via upload

---
+++ nestedternary.java
@@ -0,0 +1,11 @@
+public class ten {
+    public static void main(String[]args){
+        int num1=20;
+        int num2=30;
+        int num3=40;
+        int max;
+        max=(num1>num2)?(num1>num3) && (num2>num3)? num1:num2:num3;
+        System.out.println("Largest number is :"+max);
+    }
+    
+}
